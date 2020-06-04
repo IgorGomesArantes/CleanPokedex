f4da191a1e3c105ae866650c6192ed9473815d6e
igorgomesarantes <igorgarantes@gmail.com>
2020-06-03 21:54:05 -0300

    Initial Commit

---
+++ CleanPokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanPokedex
+//
+//  Created by Igor Gomes Arantes on 03/06/20.
+//  Copyright © 2020 Igor Gomes Arantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
